da257e7fde6c377d92d5a23dd2fcf1d3dd7b7283
zhangjq-l <zhangjq-l@glodon.com>
2025-11-16 14:01:00 +0800

    Initial commit

---
+++ dist/bundle.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const a = "这是一个测试打包压缩的文件";
+console.log(a);
+
+function test() {
+  console.log("%c 🌷🌷🌷🌷[  ]-6", "font-size:13px; background:#ecf462; color:#ffffa6;", a);
+  return "test";
+}
+
+test();
+//# sourceMappingURL=bundle.js.map
---
+++ rollup.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    input: 'test_demo/home.js',
+    output: {
+        file: 'dist/bundle.js',
+        format: 'cjs',
+        sourcemap: true
+    }
+};
